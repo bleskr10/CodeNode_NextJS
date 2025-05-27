b7cf0c1190eb5005e9e4d837885013ad62953323
bleskr10 <orobles.ro@icloud.com>
2025-05-27 23:39:06 +0200

    modified:   src/app/posts/[id]/page.tsx

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,28 +1,31 @@
-import Link from "next/link";
+export async function generateStaticParams() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await res.json();
 
-export default function PostsLayout({
-    children,
+    return posts.map((post: { id: number }) => ({
+        id: post.id.toString(),
+    }));
+}
+
+export default async function Post({
+    params,
 }: {
-    children: React.ReactNode;
+    params: { id: string };
 }) {
+    const { id } = params;
+    if (!id) {
+        throw new Error('ID del post no proporcionado');
+    }
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    if (!res.ok) {
+        throw new Error('Error al cargar el post');
+    }
+    const data = await res.json();
+
     return (
-        <div>
-            <header className="bg-blue-800 text-white p-4">
-                <h2 className="text-xl font-semibold">Sección de Posts</h2>
-                <nav className="mt-2">
-                    <ul className="flex space-x-4">
-                        <li>
-                            <Link href="/posts" className="hover:underline">Todos los Posts</Link>
-                        </li>
-                        <li>
-                            <Link href="/" className="hover:underline">Inicio</Link>
-                        </li>
-                    </ul>
-                </nav>
-            </header>
-            <main className="p-4">
-                {children}
-            </main>
+        <div className="flex flex-col gap-4 items-center justify-center min-h-screen p-8 pb-20 border">
+            <h1 className="text-2xl font-bold">{data.title}</h1>
+            <p className="text-lg">{data.body}</p>
         </div>
     );
 }
